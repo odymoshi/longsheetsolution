38188b79b88717d3c2a54288dd014d0805174810
omoshi <ody.moshi@gmail.com>
2022-09-02 11:50:02 -0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,9 @@ namespace API
             });
 
             services.AddControllers();
+            
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -54,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
